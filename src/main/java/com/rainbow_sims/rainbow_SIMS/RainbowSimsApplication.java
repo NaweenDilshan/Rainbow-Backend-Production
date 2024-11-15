fb45c6adeccfd296fb883d10923e14929a034820
Naween <naweendilshan26@gmail.com>
2024-11-15 22:44:01 +0530

    11/15 v11

--- src/main/java/com/rainbow_sims/rainbow_SIMS/RainbowSimsApplication.java
+++ src/main/java/com/rainbow_sims/rainbow_SIMS/RainbowSimsApplication.java
@@ -2,8 +2,10 @@ package com.rainbow_sims.rainbow_SIMS;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.rainbow_sims.rainbow_SIMS.repository")
 public class RainbowSimsApplication {
 
 	public static void main(String[] args) {
